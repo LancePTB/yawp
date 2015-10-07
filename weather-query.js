47f69a80665c8be929d1feeb05eb13a1df62197a
Lance Barlaan <LPTBarlaan@gmail.com>
2015-10-07 22:44:16 +0000

    Refactored

--- weather-query.js
+++ weather-query.js
@@ -1,27 +1,34 @@
 $(function(){
-  function createWeatherWidget(data){
-    return data;
-  }
-  $('#wnewsearch').on('submit',function(event){
-    var keyword = $(this).attr("action") + $("input[name=weather-keyword]").val();
-    
-    $.getJSON(keyword +"&cb=?",function(data){
-      data.RESULTS.forEach(function(element){
-      $('.wclear').append("<div class='city'> <button> "+element.name+"</button><hr></div");
-      });
+  
+  var helpers = {
+    createWeatherWidget: function (data){ 
+    },
+    clearDiv: function(){
+      $('.wclear').find('p').remove();
+      $('.wclear').find('.city').remove();
+    },
+    searchCities: function(){
+      var keyword = $(this).attr("action") + $("input[name=weather-keyword]").val();
+      helpers.clearDiv();
+      $.getJSON(keyword +"&cb=?",function(data){
+        data.RESULTS.forEach(function(element){
+        $('.wclear').append("<div class='city'> <button> "+element.name+"</button><hr></div");
+        });
 
-      $('.city').each(function(event){
-      $(this).on('click',function(){
-        weatherURL = "http://api.wunderground.com/api/06352b9690b02b4c/conditions/q/"+$(this).find('button').text().trimLeft() + ".json";
-        $.getJSON(weatherURL,function(data){
-        $('<p>'+data.current_observation.weather+'</p>').appendTo($('.wclear'));
-        $('.wclear').find('.city').remove();
+        $('.city').each(function(){
+        $(this).on('click',function(){
+          helpers.clearDiv();
+          weatherURL = "http://api.wunderground.com/api/06352b9690b02b4c/conditions/q/"+$(this).find('button').text().trimLeft() + ".json";
+          $.getJSON(weatherURL,function(data){
+          $('<p>'+data.current_observation.weather+'</p>').appendTo($('.wclear'));
+          
+          });
+        });
         });
       });
-      });
-    });
-    return false;
-  });
-});
+      return false;
+      }
+  };
 
-// data.current_observation.weather
+  $('#wnewsearch').on('submit',helpers.searchCities);
+});
